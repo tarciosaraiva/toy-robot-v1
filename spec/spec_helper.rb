e990c21d6d3d34af6fd93f277bd4bdfd593b58ba
Tarcio Saraiva <tsaraiva@dius.com.au>
2017-09-30 20:00:30 +1000

    add ability to create/remove log directory upon test execution

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,29 @@
 require 'rubygems'
+require 'fileutils'
 require 'bundler/setup'
 
 Bundler.require(:default, :test, :development)
 
 ENV["APP_ENV"] ||= "test"
 
-$:.unshift File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+lib_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+log_dir = File.expand_path(File.join(File.dirname(__FILE__), '..', 'log'))
+
+$:.unshift lib_dir
 
 require 'simulation'
 
 RSpec.configure do |config|
+  config.before(:all) do
+    # create log directory
+    FileUtils.mkdir log_dir unless Dir.exist? 'log'
+  end
+
+  config.after(:all) do
+    # remove log directory
+    FileUtils.remove_dir log_dir
+  end
+
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
